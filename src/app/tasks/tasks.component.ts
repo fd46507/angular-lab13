9cb2dcf79edfad1e1e75ff58f51c7cd06a1c16bb
fd46507 <115067216+fd46507@users.noreply.github.com>
2022-12-15 23:55:43 +0100

    6 files modified, 1 added

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from './tasks';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
+  title?: string;
+  deadline?: Date;
+  constructor(private tasksService: TasksService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  addTask() {
+    let newTask: Task = new Task();
+    newTask.title = this.title;
+    newTask.deadline = this.deadline;
+    newTask.completed = false;
+    newTask.archived = false;
+    this.tasksService.post(newTask).subscribe((response) => {
+      console.log(response);
+    });
   }
 
+  ngOnInit() {}
 }
