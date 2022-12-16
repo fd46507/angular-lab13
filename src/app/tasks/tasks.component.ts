a3da801da188f82ef98b3c1a1549f84b98dca016
fd46507 <115067216+fd46507@users.noreply.github.com>
2022-12-16 03:09:41 +0100

    3 files modified

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -10,9 +10,12 @@ import { TasksService } from './tasks.service';
 export class TasksComponent implements OnInit {
   title?: string;
   deadline?: Date;
+  retrievedData: any;
+  tasks: Array<Task> = new Array<Task>();
+
   constructor(private tasksService: TasksService) {}
 
-  addTask() {
+  addBtnClick() {
     let newTask: Task = new Task();
     newTask.title = this.title;
     newTask.deadline = this.deadline;
@@ -21,7 +24,90 @@ export class TasksComponent implements OnInit {
     this.tasksService.post(newTask).subscribe((response) => {
       console.log(response);
     });
+    this.loadElements(true);
   }
 
-  ngOnInit() {}
+  archiveCompleted() {
+    for (let i = 0; i < this.tasks.length; i++) {
+      if (this.tasks[i].completed) {
+        this.tasks[i].archived = true;
+        this.tasksService.put(this.tasks[i]);
+      }
+    }
+    this.loadElements(true);
+  }
+
+  private switchTaskChanged(task: Task) {
+    let div = document.getElementById(String(task.id));
+    if (task.completed == true) {
+      task.completed = false;
+      div.style.backgroundColor = 'white';
+    } else {
+      task.completed = true;
+      div.style.backgroundColor = '#ADADAD';
+    }
+    this.tasksService.put(task);
+  }
+
+  loadElementsOnSite(data: Object[], reload: boolean = false) {
+    if (reload == true) {
+      let mainDiv = document.getElementById('newElements');
+      mainDiv.replaceChildren();
+    }
+    this.tasks = data;
+    // const task = tasks.find(task => task.id === 589);
+    // console.log(task);
+    for (let i = 0; i < this.tasks.length; i++) {
+      let div = document.createElement('div');
+      div.style.borderStyle = 'solid';
+      div.style.borderWidth = '1px';
+      div.style.marginBottom = '25px';
+      div.id = `${this.tasks[i].id}`;
+
+      let label = document.createElement('label');
+      label.style.fontWeight = 'bold';
+      label.style.margin = '10px';
+      label.innerText = String(this.tasks[i].deadline);
+
+      let p = document.createElement('p');
+      p.innerText = this.tasks[i].title;
+      p.style.margin = '10px';
+
+      let input = document.createElement('input');
+      input.type = 'checkbox';
+      input.style.position = 'absolute';
+      input.style.right = '25px';
+      input.style.marginTop = '-37px';
+      input.id = `${this.tasks[i].id}`;
+      input.addEventListener(
+        'click',
+        this.switchTaskChanged.bind(
+          this,
+          this.tasks.find((task) => task.id === parseInt(input.id))
+        )
+      );
+      if (this.tasks[i].completed == true) {
+        div.style.backgroundColor = '#ADADAD';
+        input.checked = true;
+      }
+
+      div.appendChild(label);
+      div.appendChild(p);
+      div.appendChild(input);
+
+      let mainDiv = document.getElementById('newElements');
+      mainDiv.appendChild(div);
+    }
+    // console.log(tasks[1]);
+  }
+
+  loadElements(reload: boolean = false) {
+    this.tasksService.get().subscribe((data) => {
+      this.loadElementsOnSite(data, reload);
+    });
+  }
+
+  ngOnInit() {
+    this.loadElements();
+  }
 }
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -7,19 +7,31 @@ import { Task } from './tasks';
   providedIn: 'root',
 })
 export class TasksService {
+  retrievedData = 1;
   constructor(private http: HttpClient) {}
 
-  // public get(archived = false): Observable<Task[]> {}
+  public get(archived = false): Observable<any> {
+    return this.http.get(
+      `https://lab13.zecer.wi.zut.edu.pl/api/fd46507?archived=${archived}`
+    );
+  }
 
   public post(task: Task): Observable<any> {
     console.log(task);
     return this.http.post(
-      'https://lab13.zecer.wi.zut.edu.pl/api/fw46507',
+      'https://lab13.zecer.wi.zut.edu.pl/api/fd46507',
       task
     );
   }
 
-  // public put(task: Task): Observable<any> {}
+  public put(task: Task): Observable<any> {
+    this.http
+      .put(`https://lab13.zecer.wi.zut.edu.pl/api/fd46507/${task.id}`, task)
+      .subscribe((response) => {
+        console.log(response);
+      });
+    return;
+  }
 
   // public deleteTask(task: Task): Observable<any> {}
 }
